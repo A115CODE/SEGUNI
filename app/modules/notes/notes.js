0aa11fb429b77bdbc39370ce6f1699449503cbf3
A115CODE <jonataneleazarmc@gmail.com>
2025-02-26 13:52:38 -0600

    Build NOTES APP

--- app/modules/notes/notes.js
+++ app/modules/notes/notes.js
@@ -3,14 +3,21 @@ const FRAMEAPP = document.getElementById('FRAMEAPP');
 const NOTES = document.createElement('section');
 NOTES.classList.add('aplication');
 NOTES.id = 'NOTES';
-NOTES.innerHTML = `
-   
-        <h1>Notas Rápidas</h1>
-        <textarea id="notaInput" placeholder="Escribe tu nota aquí..."></textarea>
-        <button id="save">Guardar Nota</button>
-        <div id="notasContainer"></div>
-    
-`;
 FRAMEAPP.appendChild(NOTES);
 
+const NOTES_TXT = document.createElement("h3");
+NOTES_TXT.textContent = "Notas Rapidas";
+NOTES.appendChild(NOTES_TXT);
+
 const NOTES_FORM = document.createElement("form");
+NOTES_FORM.id = "NOTES_FORM";
+NOTES.appendChild(NOTES_FORM);
+
+const NOTES_TXTAREA = document.createElement("textarea")
+NOTES_TXTAREA.id = "NOTES_TXTAREA";
+NOTES_TXTAREA.placeholder = "Nota Rapida..."
+NOTES_FORM.appendChild(NOTES_TXTAREA);
+
+const NOTES_BUTTON = document.createElement("button");
+NOTES_BUTTON.id = "NOTES_BUTTON";
+NOTES_FORM.appendChild(NOTES_BUTTON);

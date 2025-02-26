4e96edf5a0469e30768221f4adabb02457001914
A115CODE <jonataneleazarmc@gmail.com>
2025-02-26 15:37:55 -0600

    Updates in IU NotesApp

--- app/modules/notes/notes.js
+++ app/modules/notes/notes.js
@@ -1,3 +1,11 @@
+// SupaBase
+const { createClient } = supabase;
+
+const SUPABASE_URL = 'https://dvuqnktmjvotdlnbbnhw.supabase.co';
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2dXFua3RtanZvdGRsbmJibmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTMxNzUsImV4cCI6MjA1NjA4OTE3NX0.NBjk0irYgv23oENmlKSLHJWG6fykfsI8X1hUHrVwT2Y';
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 const FRAMEAPP = document.getElementById('FRAMEAPP');
 
 const NOTES = document.createElement('section');
@@ -6,7 +14,8 @@ NOTES.id = 'NOTES';
 FRAMEAPP.appendChild(NOTES);
 
 const NOTES_TXT = document.createElement("h3");
-NOTES_TXT.textContent = "Notas Rapidas";
+NOTES_TXT.id = "NOTES_TXT";
+NOTES_TXT.textContent = "NOTAS RAPIDAS";
 NOTES.appendChild(NOTES_TXT);
 
 const NOTES_FORM = document.createElement("form");
@@ -20,4 +29,6 @@ NOTES_FORM.appendChild(NOTES_TXTAREA);
 
 const NOTES_BUTTON = document.createElement("button");
 NOTES_BUTTON.id = "NOTES_BUTTON";
+NOTES_BUTTON.type = "submit"
+NOTES_BUTTON.textContent = "Agregar";
 NOTES_FORM.appendChild(NOTES_BUTTON);

0f19d2bc022043cac039c946771d268eca91a5fa
A115CODE <jonataneleazarmc@gmail.com>
2025-03-12 22:32:05 -0600

    Create LogIn

---
+++ login/login.js
@@ -0,0 +1,22 @@
+// SupaBase
+const { createClient } = supabase;
+
+const SUPABASE_URL = 'https://dvuqnktmjvotdlnbbnhw.supabase.co';
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2dXFua3RtanZvdGRsbmJibmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTMxNzUsImV4cCI6MjA1NjA4OTE3NX0.NBjk0irYgv23oENmlKSLHJWG6fykfsI8X1hUHrVwT2Y';
+const logIn = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const { user, error } = await logIn.auth.signIn({ email, password });
+
+    if (error) {
+        alert(error.message);
+    } else {
+        window.location.href = 'main.html';
+    }
+});

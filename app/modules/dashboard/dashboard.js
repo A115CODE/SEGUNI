87a6f39c1615aed16bf60eb39647391e4728700c
A115CODE <jonataneleazarmc@gmail.com>
2025-06-04 13:38:27 -0600

    supabase_status update

--- app/modules/dashboard/dashboard.js
+++ app/modules/dashboard/dashboard.js
@@ -1,10 +1,29 @@
+// Inicializar Supabase
+const { createClient } = supabase;
+
+const Dash_URL = 'https://dvuqnktmjvotdlnbbnhw.supabase.co';
+const Dash_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2dXFua3RtanZvdGRsbmJibmh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTMxNzUsImV4cCI6MjA1NjA4OTE3NX0.NBjk0irYgv23oENmlKSLHJWG6fykfsI8X1hUHrVwT2Y';
+const supabaseClientDashboard = createClient(Dash_URL, Dash_ANON_KEY);
+
 const FRAMEAPP = document.getElementById('FRAMEAPP');
 
 const dashboard = document.createElement('div');
 dashboard.classList.add('aplication');
 dashboard.id = 'HOME';
-dashboard.innerHTML = `
-  </$>
-  <iframe src="https://supabase.com/" frameborder="0"></iframe>
-`;
 FRAMEAPP.appendChild(dashboard);
+
+const SUPABASE_STATUS = document.createElement('iframe');
+SUPABASE_STATUS.src = 'https://status.supabase.com/'
+dashboard.appendChild(SUPABASE_STATUS)
+
+function DEPLOY_CANVAS(ID) {
+  const CANVAS = document.createElement('canvas');
+  CANVAS.id = ID;
+  dashboard.appendChild(CANVAS);
+}
+
+DEPLOY_CANVAS('TIMES_APP');
+DEPLOY_CANVAS('4DX_APP');
+DEPLOY_CANVAS('NOTES_APP')
+DEPLOY_CANVAS('DB_APP')
